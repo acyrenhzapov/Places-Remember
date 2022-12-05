a086cd7f1b5eed3d56fa9084f1966000d0d294b7
intrel <a.zh.tsyrenzhapov@gmail.com>
2022-12-05 15:05:57 +0700

    Add visualization of google profile pic

--- places_remember/users/pipeline.py
+++ places_remember/users/pipeline.py
@@ -2,15 +2,16 @@ from django.shortcuts import redirect
 
 
 def get_avatar(backend, response, user=None, *args, **kwargs):
-    url = None
+    def save_avatar(response_name: str):
+        url = response.get(response_name, '')
+        if url:
+            user.avatar = url
+            user.save()
 
     if backend.name == 'vk-oauth2':
-        url = response.get('photo', '')
-
-    if url:
-        user.avatar = url
-        user.save()
-        print(user.avatar)
+        save_avatar('photo')
+    elif backend.name == "google-oauth2":
+        save_avatar('picture')
 
 
 def auth_allowed(backend, details, response, *args, **kwargs):
